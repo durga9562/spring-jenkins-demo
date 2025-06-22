9511f25da2b319ce9497b2c74d6bb883a2a665e2
durga9562 <durga9562@gmail.com>
2025-06-22 22:06:33 +0530

    jenkins-demo

---
+++ src/main/java/com/kafka/SpringJenkinsApplication.java
@@ -0,0 +1,25 @@
+package com.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Appkication started............");
+	}
+
+	public static void main(String[] args) {
+		logger.info("Appkication started............");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
